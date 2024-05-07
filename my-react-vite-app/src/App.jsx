37498a3cf35d99e2158952369c604ee03f3326ce
arthurbitarinsa <bitarinsaarthur2@gmail.com>
2024-05-07 16:49:23 +0200

    first commit, welcome and login page

---
+++ my-react-vite-app/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import React from 'react';
+import LoginPage from './components/LoginPage';
+
+
+//function App() {
+  //const [count, setCount] = useState(0)
+
+  //return (
+    //<>
+    
+     // <h1>PETBOOK</h1>
+      
+    //</>
+  //)
+//}
+const App = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default App
+
+
