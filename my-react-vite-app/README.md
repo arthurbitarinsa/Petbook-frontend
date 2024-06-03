4a1008e4a34add8a787318f64e202f0d3ebe1ade
arthurbitarinsa <bitarinsaarthur2@gmail.com>
2024-06-03 14:42:20 +0200

    Updated with the Readme

--- my-react-vite-app/README.md
+++ my-react-vite-app/README.md
@@ -1,8 +1,8 @@
-# React + Vite
+PETBOOK PROJECT
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This Petbook project draws its idea from Facebook, it is an app/site where you post and talk about your Pet, 
+The Frontend was done by React JS while the backend was done by Djanjo (Python), the Project is fully reactive and functional with an established connection between the frontend and the backend, However, the feedback from the Coach leaves a ot of work left for the frontend as the designs are not well completed regardless of the fact that there is a lot of functionalitits in the website, in addition, we agreed to continue working on this project so that we establish a complete simulation of the Facebook but this toime having pets instead of people,  
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The deployment on Netflify had some internal errors and now i am gonna deploy on Heroku and Github, however, all the branches on Github were merged and the repository is fully functional.
+ Still alot of work is left to do
+ 
--- my-react-vite-app/src/components/Home.jsx
+++ my-react-vite-app/src/components/Home.jsx
@@ -16,12 +16,6 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="nav-links">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/allpets">All Pets</Link>
-        <Link to="/postlist">Post List</Link>
-        <Link to="/feed">Feed</Link>
         <Link to="/home">Home</Link>
       </div>
       {user && (
@@ -35,7 +29,6 @@ const NavBar = () => {
 };
 
 const HomePage = () => {
-
   const backgroundImages = [
     'https://th.bing.com/th/id/R.dc1fece15d54b6ee14e4304a409700b6?rik=fodZInz1XG4J1g&pid=ImgRaw&r=0',
     'https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
@@ -43,7 +36,7 @@ const HomePage = () => {
     'https://th.bing.com/th/id/R.0b287c521a3be0370c0383139cc5236c?rik=15biTfOtJ87aMw&pid=ImgRaw&r=0',
     'https://www.thesprucepets.com/thmb/qQXwBv2EU3E3AYe03TvPbiXm0M4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/Pacific_Parrotlet-5b4c0f9046e0fb0037d13563.jpg',
     'https://th.bing.com/th/id/R.027dba257db6d62a4c2b56527d7aaae6?rik=rLgYEz0atD9tBg&pid=ImgRaw&r=0',
-     'https://fthmb.tqn.com/tvPe1GjuUCasntTK2maHH_0FBZo=/1280x853/filters:fill(auto,1)/gouldian-finch-188062467-resized-58a6ea6e5f9b58a3c9190af4.jpg'
+    'https://fthmb.tqn.com/tvPe1GjuUCasntTK2maHH_0FBZo=/1280x853/filters:fill(auto,1)/gouldian-finch-188062467-resized-58a6ea6e5f9b58a3c9190af4.jpg'
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -66,14 +59,11 @@ const HomePage = () => {
         className="body"
         style={{ backgroundImage: `url(${backgroundImages[currentImage]})` }}
       >
-  
       </div>
       <footer className="footer">
         <div className="footer-content">
-            <p>About Us</p>
+          <p>About Us</p>
           <p>All rights reserved</p>
-
-      
         </div>
       </footer>
     </div>
