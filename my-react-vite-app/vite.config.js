90e55e7c68f9941910c625809b76b7fe911210ed
arthurbitarinsa <bitarinsaarthur2@gmail.com>
2024-06-03 16:28:39 +0200

    make it better

--- my-react-vite-app/vite.config.js
+++ my-react-vite-app/vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_API_URL_LOGIN: process.env.VITE_API_URL_LOGIN,
+      VITE_API_URL_PETS: process.env.VITE_API_URL_PETS,
+      VITE_API_URL_POSTS: process.env.VITE_API_URL_POSTS,
+      VITE_API_URL_SIGNUP: process.env.VITE_API_URL_SIGNUP,
+    }
+  }
 });
+
